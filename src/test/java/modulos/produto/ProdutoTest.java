a35842677edc6ab3190f5d1a31d3313d5dd2aee2
Elton Lima <lanotle@gmail.com>
2023-06-26 22:27:57 -0300

    Removi importacoes e comentarios desnecessarios

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -2,34 +2,21 @@ package modulos.produto;
 
 import dataFactory.ProdutoDataFactory;
 import dataFactory.UsuarioDataFactory;
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pojo.ComponentePojo;
-import pojo.ProdutoPojo;
-import pojo.UsuarioPojo;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
-
 @DisplayName("Testes de API Rest do modulo de Produto")
 public class ProdutoTest {
     private String token;
-
     @BeforeEach
     public void beforeEach() {
         // Configurando os dados da API Rest da Lojinha
         baseURI = "http://165.227.93.41";
-        // port = 8080;
         basePath = "/lojinha";
-
         // Obter o token do usuario admin
         this.token = given()
                 .contentType(ContentType.JSON)
@@ -40,7 +27,6 @@ public class ProdutoTest {
                 .extract()
                     .path("data.token");
     }
-
     @Test
     @DisplayName("Validar que o valor do produto igual a 0.00 nao e permitido")
     public void testValidarLimiteZeradoProibidoValorProduto() {
@@ -57,7 +43,6 @@ public class ProdutoTest {
                     .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
                     .statusCode(422);
     }
-
     @Test
     @DisplayName("Validar que o valor do produto igual a 7000.01 nao e permitido")
     public void testValidarLimiteMaiorSeteMilProibidoValorProduto() {
